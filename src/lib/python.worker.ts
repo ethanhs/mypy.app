ea1d848e675278ff165b28eb6387cdee09e5a9bd
Ethan Smith <ethan@ethanhs.me>
2022-09-11 23:32:55 -0700

    Minimal working webapp!

---
+++ src/lib/python.worker.ts
@@ -0,0 +1,47 @@
+import { loadPyodide } from "pyodide";
+import * as Comlink from "comlink";
+
+async function loadPyodideAndPackages(version = "0.21.2") {
+    const pyodide = await loadPyodide({indexURL: `https://cdn.jsdelivr.net/pyodide/v${version}/full/`});
+    console.log("Loaded pyodide");
+    await pyodide.loadPackage("micropip")
+    let micropip = pyodide.pyimport("micropip");
+    console.log("Loaded micropip");
+    await micropip.install([
+        "typing_extensions>=3.10",
+        "mypy_extensions>=0.4.3",
+        "tomli>=1.1.0",
+    ]);
+    await micropip.install("/mypy-0.980+dev.36709e317890623feb0b2d81b02fff8ae4346b2d-cp310-cp310-emscripten_3_1_14_wasm32.whl")
+    return pyodide;
+}
+
+const pyodidePromise = loadPyodideAndPackages();
+
+class MypyWebworkerInterface {
+
+    /**
+     * ensurePyodideLoaded
+     * calls await loadPyodideandPackages
+     */
+    public async ensurePyodideLoaded() {
+        await pyodidePromise;
+    }
+
+    // Writes input to the location in the Emscripten FS
+    public async writeFile(location: string, input: string) {
+        const pyodide = await pyodidePromise;
+        pyodide.FS.writeFile(location, input);
+    }
+
+    public async runMypy(args: string[]): Promise<[number, string, string]> {
+        const pyodide = await pyodidePromise;
+        const mypy_api = pyodide.pyimport("mypy.api");
+        return mypy_api.run(pyodide.toPy(args)).toJs();
+    }
+    
+}
+
+Comlink.expose(MypyWebworkerInterface);
+
+export {};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,46 @@
-<Pulse size="60" color="#FF3E00" unit="px" duration="1s"></Pulse>
+<div>
+  {#await loadPyodide}
+  <Pulse size="60" color="#1F5082" unit="px" duration="1.5s"></Pulse>
+  {:then _}
+  <CodeMirror bind:value lang={python()} />
+  <button 
+    type="button"
+    class="inline-block px-6 py-2.5 bg-blue-600 text-white"
+    on:click="{runMypy}"></button>
+  <div class="output">
+    <p>{output}</p>
+  </div>
+  {:catch error}
+      <p>Uh-oh, an error occured!</p>
+  {/await}
+</div>
 
-<script>
-  import { Pulse } from 'svelte-loading-spinners'
+<script lang=ts>
+  import { Pulse } from 'svelte-loading-spinners';
+  import CodeMirror from "svelte-codemirror-editor";
+  import { python } from '@codemirror/lang-python';
+  import { onMount } from 'svelte';
+  import * as Comlink from "comlink";
+  let worker: Worker | null = null;
+  let output = "";
+  let mypy_instance: any = undefined;
+  let value = "'This should error' + 1";
+  const loadPyodide = (async () => {
+    const PythonWorker = await import('../lib/python.worker.ts?worker');
+    worker = new PythonWorker.default();
+    const MypyWebworker = Comlink.wrap(worker);
+    // @ts-ignore
+    mypy_instance = await new MypyWebworker();
+    return await mypy_instance.ensurePyodideLoaded();
+  })();
+  async function runMypy() {
+    // TODO: handle args
+    const tmpfile_path = "/tmp/test.py";
+    await mypy_instance.writeFile(tmpfile_path, value);
+    let [retcode, stdout, stderr] = await mypy_instance.runMypy([tmpfile_path]);
+    console.log(retcode);
+    console.log(stdout);
+    console.log(stderr);
+    output = stdout + stderr;
+  }
 </script>
