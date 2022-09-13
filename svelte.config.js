be4c385747ed226b9b08a157740ec4ad08ef0624
Ethan Smith <ethan@ethanhs.me>
2022-09-13 00:52:02 -0700

    Add vercel adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import vercel from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter({ out: 'build' }),
+		adapter: vercel(),
 	}
 };
 
