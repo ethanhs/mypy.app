57b16292596b675b6e2405f2fe454c8c6f000560
Ethan Smith <ethan@ethanhs.me>
2022-09-15 01:14:38 -0700

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # mypy.app
 
 This is a webapp which allows you to run mypy code in the browser for experimentation.
+
+Eventually, the hope is to support multiple versions of mypy.
+
+For now the main features are:
+ - entirely client-side type-checking
+ - ability to install pure-Python packages (e.g. types-* packages)
+ - pass arbitrary command line flags
+ - color output
+ - ability to share snippets with command line flags and installed packages
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,18 +3,17 @@
 	<div class="m-20 text-2xl">
 		mypy.app is a playground for experimenting with typing and mypy. It was created by <a
 			class="text-slate-800 underline"
-			href="https://twitter.com/ethanhs">Ethan Smith</a>.
-      This project would likely have taken a lot longer without the work of the
+			href="https://twitter.com/ethanhs">Ethan Smith</a
+		>. This project would likely have taken a lot longer without the work of the
 		<a class="text-slate-800 underline" href="https://pyodide.org">pyodide team</a>, many many
-		thanks to them! I also would like to thank my friends for trying the app and giving valuable
-		feedback.
+		thanks to them! The project was inspired by
+		<a class="text-slate-800 underline" href="https://mypy-play.net"
+			>the mypy playground by Yusuke Miyazaki</a
+		>I also would like to thank my friends for trying the app and giving valuable feedback.
 	</div>
 	<div class="flex items-center justify-center m-10">
-		<a
-      class="text-slate-800 underline text-2xl"
-      href="https://github.com/ethanhs/mypy.app"
-    >
-      Github link
-    </a>
+		<a class="text-slate-800 underline text-2xl" href="https://github.com/ethanhs/mypy.app">
+			Github link
+		</a>
 	</div>
 </div>
