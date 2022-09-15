9cfb3f8b59b4520c68cdc33058c060071839ee60
Ethan Smith <ethan@ethanhs.me>
2022-09-15 01:08:07 -0700

    Add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<div class="h-[calc(100%-5rem)] flex-col items-center justify-center">
+	<div class="flex items-center justify-center mt-20 text-5xl">About mypy.app</div>
+	<div class="m-20 text-2xl">
+		mypy.app is a playground for experimenting with typing and mypy. It was created by <a
+			class="text-slate-800 underline"
+			href="https://twitter.com/ethanhs">Ethan Smith</a>.
+      This project would likely have taken a lot longer without the work of the
+		<a class="text-slate-800 underline" href="https://pyodide.org">pyodide team</a>, many many
+		thanks to them! I also would like to thank my friends for trying the app and giving valuable
+		feedback.
+	</div>
+	<div class="flex items-center justify-center m-10">
+		<a
+      class="text-slate-800 underline text-2xl"
+      href="https://github.com/ethanhs/mypy.app"
+    >
+      Github link
+    </a>
+	</div>
+</div>
